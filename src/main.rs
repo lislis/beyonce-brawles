20382eb80afe58b90de7a81632ab9bca16526c46
lislis <mail@lislis.de>
2017-09-04 23:44:16 +0200

    add font and text

--- src/main.rs
+++ src/main.rs
@@ -11,19 +11,23 @@ struct MainState {
     pos_x: f32,
     pos_y: f32,
     image: graphics::Image,
-    image2: graphics::Image
+    image2: graphics::Image,
+    text: graphics::Text
 }
 
 impl MainState {
     fn new(ctx: &mut Context) -> GameResult<MainState> {
         let image = graphics::Image::new(ctx, "/b1.png").unwrap();
         let image2 = graphics::Image::new(ctx, "/b2.png").unwrap();
+        let font = graphics::Font::new(ctx, "/leaguespartan-bold.ttf", 28)?;
+        let text = graphics::Text::new(ctx, "Beyonce Brawls", &font)?;
 
         let s = MainState {
             pos_x: 400.0,
             pos_y: 0.0,
             image: image,
-            image2: image2
+            image2: image2,
+            text: text
         };
         Ok(s)
     }
@@ -41,6 +45,8 @@ impl event::EventHandler for MainState {
         let dest_point = graphics::Point::new(self.pos_x, self.pos_y);
         //graphics::circle(ctx, DrawMode::Fill, Point { x: self.pos_x, y: 380.0 }, 100.0, 32)?;
         graphics::draw(ctx, &self.image, dest_point, 0.0)?;
+
+        graphics::draw(ctx, &self.text, Point { x: self.text.width() as f32, y: self.text.height() as f32 }, 0.0)?;
         graphics::present(ctx);
         Ok(())
     }
