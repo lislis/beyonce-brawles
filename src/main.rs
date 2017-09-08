0b8574324a83198dcd30ccfa7321e66c54e29f7d
lislis <mail@lislis.de>
2017-09-08 18:33:01 +0200

    basic holdup

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,8 @@ struct MainState {
     pos_y: f32,
     image: graphics::Image,
     image2: graphics::Image,
-    text: graphics::Text
+    text: graphics::Text,
+    holding: f32
 }
 
 impl MainState {
@@ -27,15 +28,32 @@ impl MainState {
             pos_y: 0.0,
             image: image,
             image2: image2,
-            text: text
+            text: text,
+            holding: 0.0
         };
         Ok(s)
+    }
+    pub fn hold(&mut self) {
+        if self.holding > 0.0 {
+            // count time
+            self.holding += 0.1;
+            println!("Holding {}", self.holding);
+
+        } else {
+            self.holding = 0.1;
+        }
+    }
+    pub fn unhold(&mut self) -> bool {
+        self.holding = 0.0;
+        true
     }
 }
 
 impl event::EventHandler for MainState {
     fn update(&mut self, _ctx: &mut Context, _dt: Duration) -> GameResult<()> {
-        self.pos_y = self.pos_y % 400.0 + 1.0;
+        if self.holding == 0.0 {
+            self.pos_y = self.pos_y % 400.0 + 1.0;
+        }
         Ok(())
     }
 
@@ -43,14 +61,21 @@ impl event::EventHandler for MainState {
         graphics::clear(ctx);
 
         let dest_point = graphics::Point::new(self.pos_x, self.pos_y);
-        //graphics::circle(ctx, DrawMode::Fill, Point { x: self.pos_x, y: 380.0 }, 100.0, 32)?;
-        graphics::draw(ctx, &self.image, dest_point, 0.0)?;
-
         graphics::draw(ctx, &self.text, Point { x: self.text.width() as f32, y: self.text.height() as f32 }, 0.0)?;
+
+        if self.holding > 4.0 { // magic number
+            graphics::draw(ctx, &self.text, Point { x: 200.0,  y: 500.0}, 0.0)?;
+            graphics::draw(ctx, &self.image2, dest_point, 0.0)?;
+        } else if self.holding > 0.0 {
+            graphics::draw(ctx, &self.image2, dest_point, 0.0)?;
+        } else {
+            graphics::draw(ctx, &self.image, dest_point, 0.0)?;
+        }
+
         graphics::present(ctx);
         Ok(())
     }
-    
+
     fn key_down_event(&mut self, keycode: Keycode, keymod: Mod, repeat: bool) {
         println!(
             "Key pressed: {:?}, modifier {:?}, repeat: {}",
@@ -58,6 +83,12 @@ impl event::EventHandler for MainState {
             keymod,
             repeat
         );
+        match keycode {
+            Keycode::Space => {
+                self.hold();
+            }
+            _ => {}
+        }
     }
     fn key_up_event(&mut self, keycode: Keycode, keymod: Mod, repeat: bool) {
         println!(
@@ -66,6 +97,13 @@ impl event::EventHandler for MainState {
             keymod,
             repeat
         );
+
+        match keycode {
+            Keycode::Space => {
+                self.unhold();
+            }
+            _ => {}
+        }
     }
 }
 
