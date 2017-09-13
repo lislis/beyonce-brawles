7bdaf45c9c42849a20c5bce27b4ce6a0f4212e1c
lislis <mail@lislis.de>
2017-09-13 19:09:32 +0200

    create player struct

--- src/main.rs
+++ src/main.rs
@@ -7,43 +7,52 @@ use ggez::graphics;
 use ggez::graphics::{Color, DrawMode, Point};
 use std::time::Duration;
 
-struct MainState {
-    pos_x: f32,
-    pos_y: f32,
-    image: graphics::Image,
-    image2: graphics::Image,
+
+struct Player {
+    x: f32,
+    y: f32,
+    w: f32,
+    h: f32,
+    sprite1: graphics::Image,
+    sprite2: graphics::Image,
     hitarea: graphics::Image,
-    text: graphics::Text,
-    holding: f32,
-    t_x: f32,
-    t_y: f32,
-    t_image: graphics::Image
+    holding: f32
 }
 
-impl MainState {
-    fn new(ctx: &mut Context) -> GameResult<MainState> {
-        let image = graphics::Image::new(ctx, "/b1.png").unwrap();
-        let image2 = graphics::Image::new(ctx, "/b2.png").unwrap();
-        let hitarea = graphics::Image::new(ctx, "/dangerzone.png").unwrap();
-        let font = graphics::Font::new(ctx, "/leaguespartan-bold.ttf", 28)?;
-        let text = graphics::Text::new(ctx, "Beyonce Brawls", &font)?;
-
-        let thing_image = graphics::Image::new(ctx, "/thing.png").unwrap();
-
-        let s = MainState {
-            pos_x: 400.0,
-            pos_y: 0.0,
-            image: image,
-            image2: image2,
-            hitarea: hitarea,
-            text: text,
-            holding: 0.0,
-            t_x: 330.0,
-            t_y: 300.0,
-            t_image: thing_image
-        };
-        Ok(s)
+impl Player {
+    fn new(ctx: &mut Context) -> Player {
+        Player {
+            x: 400.0,
+            y: 0.0,
+            w: 64.0,
+            h: 64.0,
+            sprite1: graphics::Image::new(ctx, "/b1.png").unwrap(),
+            sprite2: graphics::Image::new(ctx, "/b2.png").unwrap(),
+            hitarea: graphics::Image::new(ctx, "/dangerzone.png").unwrap(),
+            holding: 0.0
+        }
     }
+
+    pub fn update(&mut self) {
+        if self.holding == 0.0 {
+            self.y = self.y % 400.0 + 1.0;
+        }
+    }
+
+    pub fn draw(&mut self, ctx: &mut Context) {
+        let dest_point = graphics::Point::new(self.x, self.y);
+
+        if self.holding > 4.0 { // magic number
+            let dest_hitarea = graphics::Point::new(self.x, self.y + 32.0);
+            graphics::draw(ctx, &self.sprite2, dest_point, 0.0);
+            graphics::draw(ctx, &self.hitarea, dest_hitarea, 0.0);
+        } else if self.holding > 0.0 {
+            graphics::draw(ctx, &self.sprite2, dest_point, 0.0);
+        } else {
+            graphics::draw(ctx, &self.sprite1, dest_point, 0.0);
+        }
+    }
+
     pub fn hold(&mut self) {
         if self.holding > 0.0 {
             // count time
@@ -60,16 +69,44 @@ impl MainState {
             self.holding = 0.1;
         }
     }
+
     pub fn unhold(&mut self) -> bool {
         self.holding = 0.0;
         true
     }
+}
+
+
+struct MainState {
+    player: Player,
+    text: graphics::Text,
+    t_x: f32,
+    t_y: f32,
+    t_image: graphics::Image
+}
+
+impl MainState {
+    fn new(ctx: &mut Context) -> GameResult<MainState> {
+        let font = graphics::Font::new(ctx, "/leaguespartan-bold.ttf", 28)?;
+        let text = graphics::Text::new(ctx, "Beyonce Brawls", &font)?;
+        let thing_image = graphics::Image::new(ctx, "/thing.png").unwrap();
+
+        let s = MainState {
+            player: Player::new(ctx),
+            text: text,
+            t_x: 330.0,
+            t_y: 300.0,
+            t_image: thing_image
+        };
+        Ok(s)
+    }
+
     pub fn collision(&mut self) {
-        if self.holding > 4.0 { //magic number
-            if self.pos_x < self.t_x + 128.0 &&
-                self.pos_x + 64.0 > self.t_x &&
-                self.pos_y < self.t_y + 32.0 &&
-                self.pos_y + 64.0 > self.t_y {
+        if self.player.holding > 4.0 { //magic number
+            if self.player.x < self.t_x + 128.0 &&
+                self.player.x + self.player.w > self.t_x &&
+                self.player.y < self.t_y + 32.0 &&
+                self.player.y + self.player.h > self.t_y {
                     println!("HIT EM");
                 }
         }
@@ -79,9 +116,7 @@ impl MainState {
 
 impl event::EventHandler for MainState {
     fn update(&mut self, _ctx: &mut Context, _dt: Duration) -> GameResult<()> {
-        if self.holding == 0.0 {
-            self.pos_y = self.pos_y % 400.0 + 1.0;
-        }
+        self.player.update();
         self.collision();
         Ok(())
     }
@@ -89,22 +124,14 @@ impl event::EventHandler for MainState {
     fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
         graphics::clear(ctx);
 
-        let dest_point = graphics::Point::new(self.pos_x, self.pos_y);
         graphics::draw(ctx, &self.text, Point { x: self.text.width() as f32, y: self.text.height() as f32 }, 0.0)?;
-
         graphics::draw(ctx, &self.t_image, Point { x: self.t_x, y: self.t_y }, 0.0)?;
 
-        if self.holding > 4.0 { // magic number
-            let dest_hitarea = graphics::Point::new(self.pos_x, self.pos_y + 32.0);
-            graphics::draw(ctx, &self.text, Point { x: 200.0,  y: 500.0}, 0.0)?;
-            graphics::draw(ctx, &self.image2, dest_point, 0.0)?;
-            graphics::draw(ctx, &self.hitarea, dest_hitarea, 0.0)?;
-        } else if self.holding > 0.0 {
-            graphics::draw(ctx, &self.image2, dest_point, 0.0)?;
-        } else {
-            graphics::draw(ctx, &self.image, dest_point, 0.0)?;
-        }
+        self.player.draw(ctx);
 
+        if self.player.holding > 4.0 {
+            graphics::draw(ctx, &self.text, Point { x: 200.0,  y: 500.0}, 0.0)?;
+        }
         graphics::present(ctx);
         Ok(())
     }
@@ -118,7 +145,7 @@ impl event::EventHandler for MainState {
         );
         match keycode {
             Keycode::Space => {
-                self.hold();
+                self.player.hold();
             }
             _ => {}
         }
@@ -133,7 +160,7 @@ impl event::EventHandler for MainState {
 
         match keycode {
             Keycode::Space => {
-                self.unhold();
+                self.player.unhold();
             }
             _ => {}
         }
